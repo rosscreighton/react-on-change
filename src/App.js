7038f4e372b0d8d6c79324cbe485f950933b4355
rosscreighton <rosson.creighton@gmail.com>
2016-08-16 11:39:46 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      changes: [],
+    }
+  }
+
+  onChange(e) {
+    this.setState({
+      changes: this.state.changes.concat([e])
+    })
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <div>Virtual DOM onChange:</div>
+        <input type="text" onChange={this.onChange.bind(this)} />
+        {this.state.changes.map(c => (
+          <div>onChange fired</div>
+        ))}
+      </div>
     );
   }
 }
